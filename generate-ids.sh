3e22acfacd06bd72daaff7449bf7cce5b5c634f7
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-05-07 08:28:59 -0400

    improve assets.json and looper

--- generate-ids.sh
+++ generate-ids.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+id=("1" "2" "3" "4" "5" "6")
+
+function looper() {
+        for ((s = 0 ; s <= $sub_addons - 1 ; s++)); do
+        curr_id=$(jq .repos.addons[$a].variants[$s].id assets.json)
+        id[$a]=$curr_id
+        echo ${id[*]}
+    done
+}
+
 addons=$(jq '.repos.addons | length' assets.json)
 for ((a = 0 ; a <= $addons - 1 ; a++)); do
     sub_addons=$(jq '.repos.addons['$a'].variants | length' assets.json)
-    for ((s = 0 ; s <= $sub_addons - 1 ; s++)); do
-        sub_addons=$(jq '.repos.addons['$a'].variants | length' assets.json)
-        echo $addons $sub_addons
-    done
+    looper
 done
