2ba7040f85eea98412720a2f94ee95c543bdff78
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-05-08 00:51:22 -0400

    add mkdir and cloning scripts

---
+++ clone-repos.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd /media/velvetremedy/Server-Backups/releases/repos
+
+git clone https://github.com/Love-and-Tolerance/Love-and-Tolerance.git
+git clone https://github.com/Love-and-Tolerance/Autumn-Addon.git
+git clone https://github.com/Love-and-Tolerance/Spring-Addon.git
+git clone https://github.com/Love-and-Tolerance/Winter-Addon.git
+git clone https://github.com/Love-and-Tolerance/Music-side-B.git
+git clone https://github.com/Love-and-Tolerance/3d-Models-Addon.git
+git clone https://github.com/Love-and-Tolerance/Hearts-and-Hooves-Addon.git
+git clone https://github.com/Love-and-Tolerance/Classic-Textures-Addon.git
+git clone https://github.com/Love-and-Tolerance/Bronydog-Textures-Addon.git
---
+++ mkdir.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mkdir -p /media/velvetremedy/Server-Backups/releases/{repos,zip-dir}
