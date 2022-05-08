26ffb9c9df55635562f012b2ff38f8037001b462
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-05-08 10:01:31 -0400

    switch back to optipng

--- optimize-images.sh
+++ optimize-images.sh
@@ -4,6 +4,5 @@ cd /media/velvetremedy/Server-Backups/releases/repos
 
 for file in `find -name '*.png'`
 do
-  pngcrush -reduce -brute $file /media/velvetremedy/Server-Backups/releases/tmp/crushed.png
-  mv /media/velvetremedy/Server-Backups/releases/tmp/crushed.png $file
+  optipng -o7 $file
 done
