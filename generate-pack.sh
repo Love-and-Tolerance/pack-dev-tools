622b00abe8aa338ec54493c6c457be37f1188bd0
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-05-10 02:59:07 -0400

    add zipping and bronydog addon

--- generate-pack.sh
+++ generate-pack.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
 filename=$(echo L_T-$1$2$3$4$5$6.zip | tr -d  '"')
-id=($1 $2 $3 $4 $5 $6)
+echo $filename
 cd /media/velvetremedy/Server-Backups/releases/tmp
 rm -rf *
 DATE=$(date +%Y-%m-%d-%k-%M-%S | tr -d ' ')
 cp -rt . /media/velvetremedy/Server-Backups/releases/repos/Love-and-Tolerance/{assets,LICENSE,pack.png,pack.mcmeta,README.md}
 echo "$DATE: Love & Tolerance base" > config.log
-echo ${id[0]}
-if [[ "${id[0]}" == *"a"* ]]; then
+
+pone=$(echo $1 | tr -d '"')
+ptwo=$(echo $2 | tr -d '"')
+pthree=$(echo $3 | tr -d '"')
+pfour=$(echo $4 | tr -d '"')
+pfive=$(echo $5 | tr -d '"')
+psix=$(echo $6 | tr -d '"')
+
+if [[ "$psix" == "b" ]]; then
+    DATE=$(date +%Y-%m-%d-%k-%M-%S | tr -d ' ')
+    cp -rt . /media/velvetremedy/Server-Backups/releases/repos/Bronydog-Textures-Addon/assets
+    echo "$DATE: Bronydog textures addon" >> config.log
+fi
+
+if [[ "$pone" == "a" ]]; then
     DATE=$(date +%Y-%m-%d-%k-%M-%S | tr -d ' ')
     cp -rt . /media/velvetremedy/Server-Backups/releases/repos/Autumn-Addon/assets
     echo "$DATE: Autumn addon" >> config.log
-    echo "test"
 fi
+
+
+zip -rq9 ../zip-dir/$filename *
