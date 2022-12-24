c73f61432d5d8bb5611a36d6fa2839b8c18e405b
Silk Rose <silkrose@love-tolerance.com>
2022-12-24 04:20:18 -0500

    remove old files

--- tools/release-builder/bash/mkdir.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p /media/velvetremedy/Server-Backups/releases/{repos,tmp,zip-dir,zip-dir-bedrock}
--- tools/release-builder/bash/optimize-images.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd /media/velvetremedy/Server-Backups/releases/repos
-
-for file in `find -name '*.png'`
-do
-  oxipng -o 6 -i 1 --strip safe $file
-done
