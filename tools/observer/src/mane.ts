e2bfdda683994f0a00bd77b55e5d9d01bb2bf360
Silk Rose <silkrose@love-tolerance.com>
2023-01-30 07:29:34 -0500

    observer add get changes and change separator

--- tools/observer/src/mane.ts
+++ tools/observer/src/mane.ts
@@ -6,6 +6,11 @@ const old_release =
 const new_release =
   "/home/velvetremedy/Stuff/lat_zips/resource-packs/beta-versions/Arekuzu-test-2/";
 
+let added: string[]  = [];
+let renamed: string[]  = [];
+let changed: string[]  = [];
+let deleted: string[] = [];
+
 async function mane() {
   const programs = ["git", "rsync"];
   check_installed(programs);
@@ -16,6 +21,10 @@ async function mane() {
   execute_command(`git add * && git commit -m "previous release"`);
   execute_command('rm -r `ls | grep -v "./.git"`');
   execute_command(`rsync -avP ${new_release} ./`);
+  execute_command(`git add -A`);
+  let changes = get_changes() as string;
+  separate_changes(changes);
+  console.log(added, renamed, changed, deleted);
 }
 
 function check_installed(programs: string[]) {
@@ -33,6 +42,29 @@ function execute_command(command: string) {
     execSync(command);
   } catch (err) {
     console.error(err);
+  }
+}
+
+function get_changes() {
+  try {
+    return execSync(`git status -s`).toString();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function separate_changes(changes: string) {
+  let changes_array = changes.split("\n");
+  for (let change of changes_array) {
+    if (change.charAt(0) == "A") {
+      added.push(change.slice(3, change.length));
+    } else if (change.charAt(0) == "R") {
+      renamed.push(change.slice(3, change.length));
+    } else if (change.charAt(0) == "M") {
+      changed.push(change.slice(3, change.length));
+    } else if (change.charAt(0) == "D") {
+      deleted.push(change.slice(3, change.length));
+    }
   }
 }
 
