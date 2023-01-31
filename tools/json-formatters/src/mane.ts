e17b43f679168bfd7fc415182fb11d2922e089fa
Silk Rose <silkrose@love-tolerance.com>
2023-01-31 06:45:33 -0500

    json-fmt add process, format file functions

--- tools/json-formatters/src/mane.ts
+++ tools/json-formatters/src/mane.ts
@@ -12,12 +12,13 @@ let fmt_args = ["-fmt", "-f", "-format"];
 let mini_args = ["-min", "-m", "-minify", "-mini"];
 
 async function mane() {
-  let process_type = parse_arg();
+  let process_type = parse_arg() as string;
   check_dir(pack_directory);
   process.chdir(pack_directory);
   let json_files = find_files_in_dir(`./`, ".json");
   let mcmeta_files = find_files_in_dir(`./`, ".mcmeta");
-  console.log(json_files, mcmeta_files);
+  process_files(process_type, json_files);
+  process_files(process_type, mcmeta_files);
 }
 
 function parse_arg() {
@@ -57,6 +58,24 @@ function find_files_in_dir(startPath: string, filter: string) {
     }
   }
   return results;
+}
+
+function process_files(type: string, files: string[]) {
+  if (type === "format") {
+    for (let file of files) {
+      format_file(json_fmt, file);
+    }
+  } else if (type === "minify") {
+    for (let file of files) {
+      format_file(json_minify, file);
+    }
+  }
+}
+
+function format_file(format: Function, file: string) {
+  let json = fs.readFileSync(file, { encoding: "utf-8" });
+  let formatted_json = format(json);
+  fs.writeFileSync(file, formatted_json, { encoding: "utf-8" });
 }
 
 mane();
