54f09264ec75d1b13710cd28d67f0d2d49ae62d2
Silk Rose <silkrose@love-tolerance.com>
2023-02-07 23:52:58 -0500

    add cmd with fail msg to fn lib

--- src/lib/src/pdtcmd.rs
+++ src/lib/src/pdtcmd.rs
@@ -49,3 +49,26 @@ pub fn execute_unix_command_with_return(cmd: &str) -> Output {
         output
     }
 }
+
+pub fn execute_windows_command_with_fail_msg(cmd: &str, msg: &str) {
+    let output = Command::new("cmd")
+        .args(["/C", cmd])
+        .output()
+        .unwrap_or_else(|_| panic!("{msg}"));
+
+    if !output.status.success() {
+        panic!("{msg}")
+    }
+}
+
+pub fn execute_unix_command_with_fail_msg(cmd: &str, msg: &str) {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(cmd)
+        .output()
+        .unwrap_or_else(|_| panic!("{msg}"));
+
+    if !output.status.success() {
+        panic!("{msg}")
+    }
+}
