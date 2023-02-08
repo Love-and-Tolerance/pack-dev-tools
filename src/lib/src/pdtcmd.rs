52c204d91d7fb7b20975bec545b5e95249dd3307
Silk Rose <silkrose@love-tolerance.com>
2023-02-07 20:53:59 -0500

    cmd lib add cmd status check

--- src/lib/src/pdtcmd.rs
+++ src/lib/src/pdtcmd.rs
@@ -1,31 +1,51 @@
 use std::process::{Command, Output};
 
 pub fn execute_windows_command(cmd: &str) {
-    Command::new("cmd")
+    let output = Command::new("cmd")
         .args(["/C", cmd])
         .output()
         .unwrap_or_else(|_| panic!("failed to execute: {cmd}"));
+
+    if !output.status.success() {
+        panic!("failed to execute: {cmd}")
+    }
 }
 
 pub fn execute_unix_command(cmd: &str) {
-    Command::new("sh")
+    let output = Command::new("sh")
         .arg("-c")
         .arg(cmd)
         .output()
         .unwrap_or_else(|_| panic!("failed to execute: {cmd}"));
+
+    if !output.status.success() {
+        panic!("failed to execute: {cmd}")
+    }
 }
 
 pub fn execute_windows_command_with_return(cmd: &str) -> Output {
-    return Command::new("cmd")
+    let output = Command::new("cmd")
         .args(["/C", cmd])
         .output()
         .unwrap_or_else(|_| panic!("failed to execute: {cmd}"));
+
+    if !output.status.success() {
+        panic!("failed to execute: {cmd}")
+    } else {
+        output
+    }
 }
 
 pub fn execute_unix_command_with_return(cmd: &str) -> Output {
-    return Command::new("sh")
+    let output = Command::new("sh")
         .arg("-c")
         .arg(cmd)
         .output()
         .unwrap_or_else(|_| panic!("failed to execute: {cmd}"));
+
+    if !output.status.success() {
+        panic!("failed to execute: {cmd}")
+    } else {
+        output
+    }
 }
