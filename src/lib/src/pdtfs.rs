1fe8e42363887935622229dc6215da8d827308e4
Silk Rose <silkrose@love-tolerance.com>
2023-02-07 23:21:16 -0500

    add rename fn to fs lib

--- src/lib/src/pdtfs.rs
+++ src/lib/src/pdtfs.rs
@@ -16,3 +16,7 @@ pub fn if_dir_exists_remove_and_remake_it(dir: &str) {
     if_dir_exists_remove_it(dir);
     fs::create_dir(dir).unwrap_or_else(|_| panic!("Failed to create {dir} directory."));
 }
+
+pub fn rename(dir: &str, new_dir: &str) {
+    fs::rename(dir, new_dir).unwrap_or_else(|_| panic!("Failed to rename {dir} to {new_dir}."));
+}
