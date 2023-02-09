adba9403f7b7e4265ef65605dac813b90d8010b8
Silk Rose <silkrose@love-tolerance.com>
2023-02-08 19:22:20 -0500

    pdtfs add find files function
    made with help from @Meadowsys

--- src/lib/src/pdtfs.rs
+++ src/lib/src/pdtfs.rs
@@ -20,3 +20,32 @@ pub fn if_dir_exists_remove_and_remake_it(dir: &str) {
 pub fn rename(dir: &str, new_dir: &str) {
     fs::rename(dir, new_dir).unwrap_or_else(|_| panic!("Failed to rename {dir} to {new_dir}."));
 }
+
+pub fn find_files_in_dir(
+    dir: &str,
+    recursive: bool,
+    extensions: &Option<Vec<&str>>,
+) -> Vec<String> {
+    let mut files = vec![];
+    let paths = fs::read_dir(dir).unwrap();
+    for path in paths {
+        let path = path.as_ref().unwrap().path().display().to_string();
+        if Path::new(&path).is_dir() && recursive {
+            files = [files, find_files_in_dir(&path, recursive, extensions)].concat();
+        } else if Path::new(&path).is_file() {
+            match *extensions {
+                Some(ref extensions) => {
+                    for ext in extensions {
+                        if path.ends_with(ext) {
+                            files.push(path.to_string());
+                        }
+                    }
+                }
+                None => {
+                    files.push(path);
+                }
+            }
+        }
+    }
+    files
+}
