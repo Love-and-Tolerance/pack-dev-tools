5fbb1f4fd95e8e662297589ea752ac6aade7576d
Silk Rose <silkrose@love-tolerance.com>
2023-02-11 19:37:14 -0500

    cargo fmt and clippy

--- src/tools/dedupe/src/main.rs
+++ src/tools/dedupe/src/main.rs
@@ -1,13 +1,13 @@
 use pdtlib::dedupe::dedupe;
-use std::{env, fs, io::Write};
 use std::time::SystemTime;
+use std::{env, fs, io::Write};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let dir = args[1].to_string();
     let dupes = dedupe(dir.clone());
-    let mut file_data : Vec<String> = vec![];
-    file_data.push(format!("duplicate files in {}", dir));
+    let mut file_data: Vec<String> = vec![];
+    file_data.push(format!("duplicate files in {dir}"));
     file_data.push("".to_string());
     for set in dupes {
         for item in set {
@@ -22,9 +22,11 @@ fn main() {
     #[cfg(not(target_os = "windows"))]
     let slash = "/";
 
-    let timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("Failed to get timestamp.");
+    let timestamp = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("Failed to get timestamp.");
 
-    let timestamp = format!("{:?}", timestamp).split('.').collect::<Vec<&str>>()[0].to_string();
+    let timestamp = format!("{timestamp:?}").split('.').collect::<Vec<&str>>()[0].to_string();
 
     let mut file = fs::File::create(format!(".{}duplicate_files_{}.txt", &slash, timestamp))
         .expect("Failed to create duplicates file.");
