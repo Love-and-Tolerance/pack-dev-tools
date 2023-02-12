5b6fb725f458a27b85d98142db5c7c08b116569d
Silk Rose <silkrose@love-tolerance.com>
2023-02-12 13:21:03 -0500

    comparator initial code restructure

--- src/lib/src/comparator.rs
+++ src/lib/src/comparator.rs
@@ -1,29 +1,34 @@
-use super::pdtfs::find_files_in_dir;
+use super::pdtfs::{check_if_dir_exists, find_files_in_dir};
+use super::pdthash::get_hash;
 use std::path::{Path, MAIN_SEPARATOR as SLASH};
 
-#[derive(Clone, Copy)]
-pub enum CompareType {
-    File,
-    Hash,
+pub struct FileData {
+    dir_one_presence: bool,
+    dir_two_presence: bool,
+    filename: String,
+    dir_one_hash: Option<String>,
+    dir_two_hash: Option<String>,
 }
 
 pub fn comparator(args: Vec<String>) {
-    let mut compare_type: CompareType;
-    let compare_file_args = ["-f", "-file"];
-    let compare_hash_args = ["-d", "-duplicate"];
-    if compare_file_args.contains(&args[1].to_lowercase().as_str()) {
-        compare_type = CompareType::File;
-    } else if compare_hash_args.contains(&args[1].to_lowercase().as_str()) {
-        compare_type = CompareType::Hash;
-    } else if Path::new(&args[1]).is_dir() && Path::new(&args[2]).is_dir() {
-        compare_type = CompareType::File;
-    } else if Path::new(&args[1]).is_file() && Path::new(&args[2]).is_file() {
-        compare_type = CompareType::File;
-    }
+    let dir_one = args[1].as_str();
+    check_if_dir_exists(dir_one);
+    let dir_two = args[2].as_str();
+    check_if_dir_exists(dir_two);
+    let recursive = true;
+    let dir_one_files = find_files_in_dir(dir_one, recursive, &None);
+    let dir_two_files = find_files_in_dir(dir_two, recursive, &None);
+    let results: Vec<FileData> = vec![];
 }
 
-pub fn compare_file(dir_one: &str, dir_two: &str, file: &str) -> (bool, bool) {
-    let mut dir_one_file = Path::new(&format!("{}{}", &dir_one, &file)).is_file();
-    let mut dir_two_file = Path::new(&format!("{}{}", &dir_two, &file)).is_file();
-    (dir_one_file, dir_two_file)
+pub fn compare_file(dir_one: &str, dir_two: &str, file: &str) -> FileData {
+    let dir_one_file = Path::new(&format!("{}{}", &dir_one, &file)).is_file();
+    let dir_two_file = Path::new(&format!("{}{}", &dir_two, &file)).is_file();
+    FileData {
+        dir_one_presence: dir_one_file,
+        dir_two_presence: dir_two_file,
+        filename: file.to_string(),
+        dir_one_hash: Some("test".to_string()),
+        dir_two_hash: Some("test".to_string()),
+    }
 }
