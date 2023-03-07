2d26e07c9ac68f6f30a5ebb45af8407b15944d51
Silk Rose <silkrose@love-tolerance.com>
2023-03-07 04:37:17 -0500

    blockify find closest color to all colors in block

--- src/lib/src/blockify.rs
+++ src/lib/src/blockify.rs
@@ -2,7 +2,9 @@ use super::pdtfs;
 use deltae::*;
 use image::{imageops, GenericImageView, Rgba};
 use lab::Lab;
-use std::path::MAIN_SEPARATOR as SLASH;
+use std::{cmp::Ordering, path::MAIN_SEPARATOR as SLASH};
+
+type Distance = (f64, (u32, u32, Rgba<u8>));
 
 pub fn blockify(block: String, pack: String) {
     pdtfs::check_if_dir_exists(&block);
@@ -14,19 +16,28 @@ pub fn blockify(block: String, pack: String) {
 }
 
 fn get_average_colors(blocks: Vec<String>) {
-    let mut averages: Vec<(i8, String)>;
+    let mut averages: Vec<(Rgba<u8>, String)>;
     for image in blocks {
         let img = image::open(&image).unwrap_or_else(|_| panic!("Failed to load image: {image}"));
         if img.dimensions().0 != img.dimensions().1 {
             continue;
         }
+        let pixel_count: f64 = (img.dimensions().0 * img.dimensions().1).into();
+        let mut distances: Vec<Distance> = vec![];
+        let mut delta: f64 = 0.0;
         for pixel in img.pixels() {
             let lab = get_lab(pixel);
+            let mut distance: f64 = 0.0;
             for sub_pixel in img.pixels() {
                 let sub_lab = get_lab(sub_pixel);
-                let de0 = DeltaE::new(&lab, &sub_lab, DE2000);
+                delta = DeltaE::new(lab, sub_lab, DE2000).value().to_owned().into();
+                distance += delta;
             }
+            distance /= pixel_count;
+            distances.push((distance, pixel));
         }
+        distances.sort_by(compare_distances);
+        println!("{:?}, {}", distances[0], image);
     }
 }
 
@@ -38,4 +49,13 @@ fn get_lab(pixel: (u32, u32, Rgba<u8>)) -> LabValue {
         a: lab.a,
         b: lab.b,
     }
+}
+
+fn compare_distances(a: &Distance, b: &Distance) -> Ordering {
+    if a.0 < b.0 {
+        return Ordering::Less;
+    } else if a.0 > b.0 {
+        return Ordering::Greater;
+    }
+    return Ordering::Equal;
 }
