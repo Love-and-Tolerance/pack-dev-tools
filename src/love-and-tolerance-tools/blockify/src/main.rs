0a6acc4be4abd05d39651b5557dba93434d3bde3
Meadowsys <blazeykirin@gmail.com>
2023-03-08 11:14:50 -0800

    add some nicer panic messages to blockify executable

--- src/love-and-tolerance-tools/blockify/src/main.rs
+++ src/love-and-tolerance-tools/blockify/src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let blocks_location = args[1].to_string();
-    let pack_location = args[2].to_string();
-    blockify(blocks_location, pack_location);
+    let blocks_location = args.get(1).expect("first arg should be location of assets/minecraft/textures/block");
+    let pack_location = args.get(2).expect("second arg should be location of assets (dir in a pack)");
+    blockify(blocks_location.into(), pack_location.into());
 }
