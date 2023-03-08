a70df5f8312973e7128dd5c3ecc1a3f189b5ffb4
Silk Rose <silkrose@love-tolerance.com>
2023-03-08 02:48:42 -0500

    blockify add pixel placing, random matches
    if pixel matched more than 1 block, it selects a random match

--- src/lib/src/blockify.rs
+++ src/lib/src/blockify.rs
@@ -3,6 +3,7 @@ use deltae::*;
 use fs_extra::dir::{copy, CopyOptions};
 use image::{imageops, GenericImageView, ImageBuffer, Rgba, RgbaImage};
 use lab;
+use rand::seq::SliceRandom;
 use std::{cmp::Ordering, path::MAIN_SEPARATOR as SLASH};
 
 type Average = (LabValue, String);
@@ -76,17 +77,19 @@ fn compare_distances(a: &Distance, b: &Distance) -> Ordering {
 }
 
 fn blockify_images(images: Vec<String>, blocks: Vec<Average>) {
-    let mut pixels: u128 = 0;
     for texture in images {
+        println!("{}", &texture);
         let img =
             image::open(&texture).unwrap_or_else(|_| panic!("Failed to load image: {texture}"));
         let (width, height) = img.dimensions();
-        let new_texture: RgbaImage =
+        let mut new_texture: RgbaImage =
             ImageBuffer::from_fn(width * 16, height * 16, |_, _| image::Rgba([0, 0, 0, 0]));
         for pixel in img.pixels() {
             if pixel.2 .0[3] == 0 {
                 continue;
             }
+            let (x, y) = (pixel.0, pixel.1);
+            let alpha = pixel.2 .0[3];
             let mut distances: Vec<(f64, String)> = vec![];
             let lab = get_lab(pixel);
             for block in &blocks {
@@ -94,9 +97,35 @@ fn blockify_images(images: Vec<String>, blocks: Vec<Average>) {
                 distances.push((delta, block.1.to_owned()));
             }
             distances.sort_by(compare_block_distances);
+            let matches = distances
+                .iter()
+                .filter(|item| item.0 == distances[0].0)
+                .collect::<Vec<&(f64, String)>>();
+            let mut selected: String;
+            if matches.len() == 1 {
+                selected = matches[0].1.to_owned();
+            } else {
+                selected = matches
+                    .choose(&mut rand::thread_rng())
+                    .unwrap()
+                    .1
+                    .to_owned();
+            }
+            let block_img = image::open(&selected)
+                .unwrap_or_else(|_| panic!("Failed to load image: {selected}"));
+            for sub_pixel in block_img.pixels() {
+                let sub_x = (x * 16) + sub_pixel.0;
+                let sub_y = (y * 16) + sub_pixel.1;
+                let rgba = Rgba::from([
+                    sub_pixel.2 .0[0],
+                    sub_pixel.2 .0[1],
+                    sub_pixel.2 .0[2],
+                    alpha,
+                ]);
+                new_texture.put_pixel(sub_x, sub_y, rgba);
+            }
         }
-        pixels += u128::from(width * height);
-        println!("{pixels}");
+        new_texture.save(texture).unwrap();
     }
 }
 
