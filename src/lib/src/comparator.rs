f559d779f3ded03360ac14d622c2480668f642b9
Silk Rose <silkrose@love-tolerance.com>
2023-03-11 12:17:13 -0500

    comparator refactor, add multithread find files fn

--- src/lib/src/comparator.rs
+++ src/lib/src/comparator.rs
@@ -11,30 +11,34 @@ pub struct FileData {
     dir_two_hash: Option<String>,
 }
 
+pub enum Structure {
+    Ordered,
+    Unordered,
+}
+
 pub fn comparator(args: Vec<String>) {
-    let dir_one = pdtfs::check_dir_ends_with_slash(args[1].clone());
-    pdtfs::check_if_dir_exists(&dir_one);
-    let dir_two = pdtfs::check_dir_ends_with_slash(args[2].clone());
-    pdtfs::check_if_dir_exists(&dir_two);
+    let mut dirs = vec![];
+    let mut order = Structure::Ordered;
+    let ordered_options = ["-o", "--ordered"];
+    let unordered_options = ["-u", "--unordered"];
+    if ordered_options.contains(&args[1].to_lowercase().as_str()) {
+        dirs = args[2..].to_vec();
+    } else if unordered_options.contains(&args[1].to_lowercase().as_str()) {
+        dirs = args[2..].to_vec();
+        order = Structure::Unordered;
+    } else {
+        dirs = args[1..].to_vec();
+    }
+    dirs = dirs
+        .iter()
+        .map(|d| {
+            pdtfs::check_if_dir_exists(&d);
+            pdtfs::check_dir_ends_with_slash(d.to_string())
+        })
+        .collect::<Vec<String>>();
+
     let recursive = true;
-    let dir_one_files = pdtfs::find_files_in_dir(&dir_one, recursive, &None)
-        .iter()
-        .map(|file| file[dir_one.chars().count()..].to_string())
-        .collect::<Vec<String>>();
-    let dir_two_files = pdtfs::find_files_in_dir(&dir_two, recursive, &None)
-        .iter()
-        .map(|file| file[dir_two.chars().count()..].to_string())
-        .collect::<Vec<String>>();
-    let mut files: Vec<String> = [dir_one_files, dir_two_files].concat();
-    files.sort();
-    files.dedup();
-    let mut results: Vec<FileData> = vec![];
-    for file in files {
-        results.push(compare_file(&dir_one, &dir_two, file));
-    }
-    for result in results {
-        println!("{result:#?}")
-    }
+    let files = pdtfs::find_files_in_multiple_dirs(dirs, recursive, &None, &true);
 }
 
 pub fn compare_file(dir_one: &str, dir_two: &str, filename: String) -> FileData {
--- src/lib/src/pdtfs.rs
+++ src/lib/src/pdtfs.rs
@@ -1,5 +1,7 @@
+use super::pdtthread;
 use std::fs;
 use std::path::{Path, MAIN_SEPARATOR as SLASH};
+use std::sync::{Arc, Mutex};
 
 pub fn check_if_dir_exists(dir: &str) {
     if !Path::new(dir).is_dir() {
@@ -30,7 +32,7 @@ pub fn find_files_in_dir(
     let mut files = vec![];
     let paths = fs::read_dir(dir).unwrap();
     for path in paths {
-        let path = path.as_ref().unwrap().path().display().to_string();
+        let path = path.unwrap().path().to_str().unwrap().to_string();
         if Path::new(&path).is_dir() && recursive {
             files = [files, find_files_in_dir(&path, recursive, extensions)].concat();
         } else if Path::new(&path).is_file() {
@@ -57,4 +59,30 @@ pub fn check_dir_ends_with_slash(dir: String) -> String {
     } else {
         format!("{dir}{SLASH}")
     }
+}
+
+pub fn find_files_in_multiple_dirs(
+    dirs: Vec<String>,
+    recursive: bool,
+    extensions: &'static Option<Vec<&str>>,
+    exclude_dir_name: &'static bool,
+) -> Vec<String> {
+    let files = Arc::new(Mutex::new(Vec::new()));
+    let dirs = dirs.into_iter().map(|d| (d, Arc::clone(&files))).collect();
+
+    pdtthread::multithread(dirs, None, move |thread_num, (dir, files)| {
+        println!("[thread {thread_num:02}] finding files in dir: {}", dir);
+        let dir_files = if *exclude_dir_name {
+            find_files_in_dir(&dir, recursive, &extensions)
+                .iter()
+                .map(|f| f[dir.chars().count()..].to_string())
+                .collect()
+        } else {
+            find_files_in_dir(&dir, recursive, &extensions)
+        };
+        dir_files
+            .iter()
+            .for_each(|f| files.lock().unwrap().push(f.to_string()))
+    });
+    Arc::try_unwrap(files).ok().unwrap().into_inner().unwrap()
 }
