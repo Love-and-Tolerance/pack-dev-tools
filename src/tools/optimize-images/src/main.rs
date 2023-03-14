b992b395c4db4a8e16fd168fda60d214cbff9fcf
Silk Rose <silkrose@love-tolerance.com>
2023-03-14 03:30:49 -0400

    optimize images clap argument parser
    most of the work done by @Meadowsys

--- src/tools/optimize-images/src/main.rs
+++ src/tools/optimize-images/src/main.rs
@@ -1,9 +1,36 @@
+use camino::Utf8PathBuf;
+use clap::{value_parser, Parser, ValueEnum};
 use pdtlib::optimize_images::optimize_images;
 use pdtlib::pdtstdin;
-use std::env;
+
+#[derive(Debug, Parser)]
+#[command(name = "optimize_images", bin_name = "optimize_images", version)]
+struct Args {
+	/// Compression level [possible values: 0 - 6]
+	#[arg(short, long, default_value_t = 6, value_parser = value_parser!(u8).range(0..=6))]
+	level: u8,
+	#[arg(short, long, value_enum, default_value_t = Strip::Safe)]
+	strip: Strip,
+	/// Try to fix errors when decoding the input file
+	#[arg(long, short)]
+	fix: bool,
+	/// Enable Adam7 interlacing
+	#[arg(long, short)]
+	interlace: bool,
+	/// List of files and folders to optimize
+	paths: Vec<Utf8PathBuf>,
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum Strip {
+	None,
+	Safe,
+	All,
+}
 
 fn main() {
-	let dir = env::args().collect::<Vec<String>>()[1].to_string();
+	let args = Args::parse();
+	println!("{args:?}");
 	let stdin = pdtstdin::get_stdin().unwrap_or(["test".to_string()].to_vec());
 
 	for thing in stdin {
@@ -11,10 +38,3 @@ fn main() {
 	}
 	//optimize_images(dir);
 }
-
-// arg options
-// -l --level (0 to 6) default 6
-// -s --strip (none, safe, all) default safe.
-// -f --fix (true or false) default true
-// -i --interlace (true or false) default true
-// everything after the arguments should be a list of files/folders
