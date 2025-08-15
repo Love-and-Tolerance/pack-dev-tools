19e94667c7f841b978ef30ad9cc2fd2d572a6049
Silk Rose <silkrose@love-tolerance.com>
2025-08-15 04:22:32 -0400

    Refactor format JSON binary

--- src/bin/json_format.rs
+++ src/bin/json_format.rs
@@ -1,120 +1,81 @@
 use camino::Utf8Path;
-use pdt::pdtfs;
-use serde::ser::Serialize;
-use serde_json::{Serializer, Value, ser::PrettyFormatter};
-use std::any::{Any, TypeId};
-use std::env;
-use std::fs;
-use std::path::Path;
+use indoc::printdoc;
+use pony::fs::find_files_in_dir;
+use pony::json::{JsonFormat, format_json};
+use pony::regex::matches;
+use regex::Regex;
+use std::process::exit;
+use std::{env, fs};
 
-fn main() {
-	let mut fmt_type = Json::Format;
-	let mut indent = Indent::Tab;
-	let format_args = ["-f", "-format"];
-	let minify_args = ["-m", "-minify"];
-	let tab_indent_args = ["-t", "-tab"];
-	let space_indent_args = ["-s", "-space"];
-	let args: Vec<String> = env::args().collect();
-	let mut i = 1;
-	while i < args.len() {
-		if Path::new(&args[i]).is_dir() || Path::new(&args[i]).is_file() {
-			json_formatter(args[i].to_owned(), fmt_type, indent);
-		} else if format_args.contains(&args[i].to_lowercase().as_str()) {
-			fmt_type = Json::Format;
-			if tab_indent_args.contains(&args[i + 1].to_lowercase().as_str()) {
-				indent = Indent::Tab;
-			} else if space_indent_args.contains(&args[i + 1].to_lowercase().as_str()) {
-				(i, indent) = parse_space_indent(i, args.clone(), 2);
-			}
-		} else if minify_args.contains(&args[i].to_lowercase().as_str()) {
-			fmt_type = Json::Minify;
-		} else if tab_indent_args.contains(&args[i].to_lowercase().as_str()) {
-			indent = Indent::Tab;
-		} else if space_indent_args.contains(&args[i].to_lowercase().as_str()) {
-			(i, indent) = parse_space_indent(i, args.clone(), 1);
-		}
-		i += 1;
+type Result<T, E = Box<dyn ::std::error::Error>> = ::std::result::Result<T, E>;
+
+fn main() -> Result<()> {
+	let args: Vec<String> = env::args().skip(1).collect();
+	let format = parse_argument(&args)?;
+	let dir = args.last().unwrap();
+	if !Utf8Path::new(dir).is_dir() {
+		return Err("Last argument is not a directory!".into());
 	}
-}
-
-fn parse_space_indent(mut i: usize, args: Vec<String>, num: usize) -> (usize, Indent) {
-	let indent: Indent;
-	if args[i + 1].parse::<usize>().is_ok()
-		&& args[i + num].parse::<usize>().unwrap().type_id() == TypeId::of::<usize>()
-	{
-		indent = Indent::Space(args[i + num].parse::<u8>().unwrap_or_else(|_| {
-			panic!("Failed to parse to u8."); // help go here.
-		}));
-		if let Indent::Space(num) = indent
-			&& !(1..=16).contains(&num)
-		{
-			panic!("Num of spaces out of bounds."); // help go here.
-		}
-		i += num;
-	} else {
-		indent = Indent::Space(2);
-	}
-	(i, indent)
-}
-
-#[derive(Copy, Clone)]
-pub enum Json {
-	Format,
-	Minify,
-}
-
-#[derive(Copy, Clone)]
-enum Indent {
-	Tab,
-	Space(u8),
-}
-
-fn json_formatter(dir_or_file: String, fmt_type: Json, indent: Indent) {
-	let recursive = true;
-	let extensions = Some(vec![".json".to_string(), ".mcmeta".to_string()]);
-	let mut files = vec![];
-	if Utf8Path::new(&dir_or_file).is_dir() {
-		files = pdtfs::find_files_in_dir(&dir_or_file, recursive, &extensions);
-	} else if Utf8Path::new(&dir_or_file).is_file() {
-		files.push(dir_or_file);
-	}
-	let indent: String = match indent {
-		Indent::Tab => "\t".to_string(),
-		Indent::Space(indent_number) => " ".repeat(indent_number as usize),
-	};
+	let includes = &Some(Regex::new(r".*\.(json|mcmeta)$")?);
+	let files = find_files_in_dir(dir, true)?;
+	let files = files
+		.iter()
+		.filter(|file| matches(file, includes, &None))
+		.collect::<Vec<_>>();
 	for file in files {
-		let mut json_data = fs::read_to_string(&file).expect("Failed to read file to string.");
-		match fmt_type {
-			Json::Format => {
-				json_data = format_json(&json_data, &indent);
+		let json = fs::read_to_string(file)?;
+		let text = format_json(&json, &format)?;
+		fs::write(file, text.as_bytes())?;
+	}
+	Ok(())
+}
+
+fn parse_argument(args: &[String]) -> Result<JsonFormat> {
+	if args.is_empty() {
+		return Err("No arguments provided!".into());
+	}
+	match args.iter().next().unwrap().as_str() {
+		"-m" | "--minify" => Ok(JsonFormat::Minify),
+		"-t" | "--tab" => Ok(JsonFormat::Tab),
+		"-s" | "--space" => match args.iter().next() {
+			None => Err("No space count provided!".into()),
+			Some(count) => {
+				let count = count.parse::<u8>();
+				match count {
+					Ok(count) => Ok(JsonFormat::Space(count)),
+					Err(_) => Err("Failed to parse space count".into()),
+				}
 			}
-			Json::Minify => {
-				json_data = minify_json(&json_data);
-			}
+		},
+		"-h" | "--help" => {
+			print_help();
+			exit(0);
 		}
-		std::fs::write(file, json_data).expect("Failed to write json to file.");
+		"-v" | "--version" => {
+			println!("{} {}", env!("CARGO_BIN_NAME"), env!("CARGO_PKG_VERSION"));
+			exit(0);
+		}
+		_ => Ok(JsonFormat::Tab),
 	}
 }
 
-fn format_json(json: &str, indent: &str) -> String {
-	let value = parse_to_value(json);
-	// json.len() is not ideal but its a _goodish_ default
-	let mut writer = Vec::with_capacity(json.len());
-	let formatter = PrettyFormatter::with_indent(indent.as_bytes());
-	let mut serialiser = Serializer::with_formatter(&mut writer, formatter);
-	value
-		.serialize(&mut serialiser)
-		.expect("Failed to serialize json data.");
-	writer.push(b'\n');
-	String::from_utf8(writer).expect("Failed to convert utf8 to string.")
-}
+fn print_help() {
+	printdoc! {"
+		{} {}
 
-fn minify_json(json: &str) -> String {
-	let value = parse_to_value(json);
-	serde_json::to_string(&value).expect("Failed to stringify json.")
-}
+		Format .json and .mcmeta files with tabs or spaces.
 
-#[inline]
-fn parse_to_value(json: &str) -> Value {
-	serde_json::from_str(json).expect("Failed to parse json.")
+		Usage Examples:
+		  json_format ./dir
+		  json_format -s 3 ./dir
+
+		Options:
+		  -m,  --minify      Format onto one line
+		  -s,  --space       Format with spaces (1-8)
+		  -t,  --tab         Format with tabs (default)
+		  -h,  --help        Print help
+		  -v,  --version     Print version\n",
+		env!("CARGO_BIN_NAME"),
+		env!("CARGO_PKG_VERSION")
+	}
 }
