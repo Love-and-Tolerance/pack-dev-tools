ee4e70a2d7f335febb2e76836469919dbc2bab89
Silk Rose <silkrose@love-tolerance.com>
2025-08-16 06:15:57 -0400

    Add temporary testing code for optimize images

--- src/bin/optimize_images.rs
+++ src/bin/optimize_images.rs
@@ -39,6 +39,47 @@ fn optimize_images(dir: &str) -> Result<()> {
 		let input = InFile::Path(Utf8PathBuf::from(&image).into());
 		let output = OutFile::from_path(Utf8PathBuf::from(&image).into());
 		optimize(&input, &output, &options)?;
+	}
 	Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+	use camino::Utf8Path;
+	use pony::bytes::{FormatType, format_size_bytes};
+
+	use super::*;
+	use std::fs;
+
+	#[test]
+	fn test_compression() -> Result<()> {
+		if Utf8Path::new("/home/velvetremedy/oitd/out").is_dir() {
+			fs::remove_dir_all("/home/velvetremedy/oitd/out")?;
+		}
+		fs::create_dir_all("/home/velvetremedy/oitd/out")?;
+		let files = find_files_in_dir("/home/velvetremedy/oitd/base", true)?;
+		for file in &files {
+			fs::copy(file, file.replace("/base/", "/out/"))?;
+		}
+		optimize_images("/home/velvetremedy/oitd/out")?;
+		let base_size = format_size_bytes(count_size(&files)? as f64, FormatType::Abbreviation)?;
+		println!("Base files: {base_size}");
+		let files = find_files_in_dir("/home/velvetremedy/oitd/out", true)?;
+		let new_size = format_size_bytes(count_size(&files)? as f64, FormatType::Abbreviation)?;
+		println!("Compressed files: {new_size}");
+		Ok(())
+	}
+
+	fn count_size(files: &[String]) -> Result<usize> {
+		let bytes = files
+			.iter()
+			.filter(|file| file.ends_with(".png"))
+			.map(|file| {
+				Ok::<_, Box<dyn std::error::Error>>(fs::File::open(file)?.metadata()?.len() as usize)
+			})
+			.collect::<Result<Vec<_>, _>>()?
+			.into_iter()
+			.sum();
+		Ok(bytes)
 	}
 }
