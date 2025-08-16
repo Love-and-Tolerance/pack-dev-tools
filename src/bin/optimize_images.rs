76f8ee0391d811c9579df254021eb0a46f21eb2e
Silk Rose <silkrose@love-tolerance.com>
2025-08-16 06:15:07 -0400

    Refactor optimize images binary

--- src/bin/optimize_images.rs
+++ src/bin/optimize_images.rs
@@ -1,77 +1,44 @@
 use camino::Utf8PathBuf;
-use clap::ValueEnum;
-use clap::{Parser, value_parser};
-use oxipng::{InFile, Options, OutFile, optimize};
-use pdt::pdtfs::get_files_in_list;
-use pony::stdin::get_stdin;
-use pony::traits::BasicVector;
-use std::path::MAIN_SEPARATOR as SLASH;
+use oxipng::{InFile, IndexSet, Options, OutFile, RowFilter, optimize};
+use pony::fs::find_files_in_dir;
+use std::{env::args, num::NonZero};
 
-#[derive(Debug, Parser)]
-#[command(
-	name = env!("CARGO_PKG_NAME"),
-	bin_name = env!("CARGO_BIN_NAME"),
-	version,
-	about = format!("Optimize all png files in given path(s).
+type Result<T, E = Box<dyn ::std::error::Error>> = ::std::result::Result<T, E>;
 
-example: .{s}optimize-images -l4 a.png .{s}assets{s}
-example: .{s}optimize-images --strip safe a.png b.png", s = SLASH),
-	long_about = None
-)]
-struct Args {
-	/// Compression level [possible values: 0 - 6]
-	#[arg(short, long, default_value_t = 6, value_parser = value_parser!(u8).range(0..=6))]
-	level: u8,
-	#[arg(short, long, value_enum, default_value_t = Strip::Safe)]
-	strip: Strip,
-	/// Try to fix errors when decoding the input file
-	#[arg(long, short)]
-	fix: bool,
-	/// Enable Adam7 interlacing
-	#[arg(long, short)]
-	interlace: bool,
-	/// List of files and folders to optimize
-	paths: Vec<String>,
+fn main() -> Result<()> {
+	let args = args().collect::<Vec<_>>();
+	optimize_images(&args[1])?;
+	Ok(())
 }
 
-fn main() {
-	let args = Args::parse();
-	let paths = get_stdin()
-		.unwrap_or_default()
-		.split(" ")
-		.map(String::from)
-		.collect::<Vec<_>>()
-		.extend_vec(args.paths);
-	optimize_images(args.level, args.strip, args.fix, args.interlace, paths);
-}
-
-#[derive(Clone, Debug, ValueEnum)]
-enum Strip {
-	None,
-	Safe,
-	All,
-}
-
-fn optimize_images(level: u8, strip: Strip, fix: bool, interlace: bool, paths: Vec<String>) {
-	let mut options = Options::from_preset(level);
-	options.strip = match strip {
-		Strip::None => oxipng::StripChunks::None,
-		Strip::Safe => oxipng::StripChunks::Safe,
-		Strip::All => oxipng::StripChunks::All,
+fn optimize_images(dir: &str) -> Result<()> {
+	let mut options = Options::from_preset(6);
+	options.deflate = oxipng::Deflaters::Zopfli {
+		iterations: NonZero::new(255).unwrap(),
 	};
-	options.fix_errors = fix;
-	options.interlace = match interlace {
-		true => Some(oxipng::Interlacing::Adam7),
-		false => None,
-	};
+	options.fix_errors = true;
+	options.optimize_alpha = true;
+	options.interlace = Some(oxipng::Interlacing::None);
+	options.strip = oxipng::StripChunks::All;
+	options.fast_evaluation = true;
+	options.filter = IndexSet::from([
+		RowFilter::Sub,
+		RowFilter::Up,
+		RowFilter::Average,
+		RowFilter::Paeth,
+		RowFilter::MinSum,
+		RowFilter::Entropy,
+		RowFilter::Bigrams,
+		RowFilter::BigEnt,
+		RowFilter::Brute,
+	]);
 	let recursive = true;
-	let extensions = Some(vec![".png".to_string()]);
-	const EXCLUDE_DIR_NAME: bool = false;
-	let images = get_files_in_list(paths, recursive, extensions, EXCLUDE_DIR_NAME, true);
-	for image in images {
+	let images = find_files_in_dir(dir, recursive)?;
+	for image in images.iter().filter(|file| file.ends_with("png")) {
 		println!("optimizing image: {}", &image);
 		let input = InFile::Path(Utf8PathBuf::from(&image).into());
 		let output = OutFile::from_path(Utf8PathBuf::from(&image).into());
-		optimize(&input, &output, &options).expect("Failed to optimize image.");
+		optimize(&input, &output, &options)?;
+	Ok(())
 	}
 }
