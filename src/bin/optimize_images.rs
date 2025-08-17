63cd60af7cbb48a009a205ca6cf4d528094ca15f
Silk Rose <silkrose@love-tolerance.com>
2025-08-17 04:06:26 -0400

    Adjust optimize images for best performance
    Disable oxipng features, adds better multi-threading, and adjusts some settings.

--- src/bin/optimize_images.rs
+++ src/bin/optimize_images.rs
@@ -1,7 +1,7 @@
 use camino::Utf8PathBuf;
-use oxipng::{InFile, IndexSet, Options, OutFile, RowFilter, optimize};
-use pony::fs::find_files_in_dir;
-use std::{env::args, num::NonZero};
+use oxipng::{InFile, Options, OutFile, optimize};
+use pony::{fs::find_files_in_dir, threads::multithread};
+use std::env::args;
 
 type Result<T, E = Box<dyn ::std::error::Error>> = ::std::result::Result<T, E>;
 
@@ -13,33 +13,22 @@ fn main() -> Result<()> {
 
 fn optimize_images(dir: &str) -> Result<()> {
 	let mut options = Options::from_preset(6);
-	options.deflate = oxipng::Deflaters::Zopfli {
-		iterations: NonZero::new(255).unwrap(),
-	};
 	options.fix_errors = true;
 	options.optimize_alpha = true;
-	options.interlace = Some(oxipng::Interlacing::None);
 	options.strip = oxipng::StripChunks::All;
-	options.fast_evaluation = true;
-	options.filter = IndexSet::from([
-		RowFilter::Sub,
-		RowFilter::Up,
-		RowFilter::Average,
-		RowFilter::Paeth,
-		RowFilter::MinSum,
-		RowFilter::Entropy,
-		RowFilter::Bigrams,
-		RowFilter::BigEnt,
-		RowFilter::Brute,
-	]);
 	let recursive = true;
-	let images = find_files_in_dir(dir, recursive)?;
-	for image in images.iter().filter(|file| file.ends_with("png")) {
+	let files = find_files_in_dir(dir, recursive)?;
+	let images = files
+		.into_iter()
+		.filter(|file| file.ends_with("png"))
+		.collect::<Vec<_>>();
+	multithread(images, None, move |_, image| {
 		println!("optimizing image: {}", &image);
 		let input = InFile::Path(Utf8PathBuf::from(&image).into());
 		let output = OutFile::from_path(Utf8PathBuf::from(&image).into());
-		optimize(&input, &output, &options)?;
-	}
+		optimize(&input, &output, &options).unwrap();
+		None::<()>
+	});
 	Ok(())
 }
 
